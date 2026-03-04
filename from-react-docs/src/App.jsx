05778a8d5a66c7150f6765da908c174c4d530b1d
Banafo <surmjnr@gmail.com>
2026-03-04 08:44:09 +0000

    initial commit

---
+++ from-react-docs/src/App.jsx
@@ -0,0 +1,15 @@
+import Smallfu from './assets/components/small'
+import { Newbutton } from './assets/components/small'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Newbutton />
+      <Smallfu  />
+    </>
+  )
+}
+
+export default App
---
+++ from-react-docs/src/assets/components/small.jsx
@@ -0,0 +1,17 @@
+
+
+
+export function Newbutton() {
+  return (
+    <button> new button </button>
+  );
+}
+
+export default function Smallfu() {
+  return (
+    <>
+      <p>Small component with new button</p>
+      <Newbutton />
+    </>
+  );
+}
