c3b11716b9bf2160164a9f64f80b4b91d80153e4
Banafo <surmjnr@gmail.com>
2026-03-04 09:55:52 +0000

    added image to build a profile

--- from-react-docs/src/App.jsx
+++ from-react-docs/src/App.jsx
@@ -1,5 +1,4 @@
 import Smallfu from './assets/components/small'
-import { Newbutton } from './assets/components/small'
 import './assets/components/small.css'
 import './App.css'
 
@@ -7,7 +6,6 @@ function App() {
 
   return (
     <>
-      <Newbutton />
       <Smallfu  />
     </>
   )
--- from-react-docs/src/assets/components/small.jsx
+++ from-react-docs/src/assets/components/small.jsx
@@ -1,5 +1,4 @@
-
-
+import banafoImage from '../pictures/Banafo.jpeg';
 
 export function Newbutton() {
   return (
@@ -7,11 +6,42 @@ export function Newbutton() {
   );
 }
 
+const userInfo ={
+  name:"Kofi",
+  age: 4,
+  country: "Ghana",
+  image: banafoImage,
+  imageSize: 100,
+  imageWidth: 100,
+};
+export function ProfileInfo(){
+  return(
+    <div className='profileInformation'>
+      <div className='content'>
+      <img src={userInfo.image} alt="profile picture"
+      style={
+        {
+        width:userInfo.imageWidth,
+        height: userInfo.imageSize,
+        borderRadius: "50%",
+        }
+    } />
+    <p>My name is {userInfo.name}</p>
+      </div>
+    
+    </div>
+
+  );
+}
+
 export default function Smallfu() {
   return (
-    <>
+    <div className='profileInformation'>
+      <div className='content'>
+      <ProfileInfo />
       <p>Small component with new button</p>
       <Newbutton />
-    </>
+      </div>
+    </div>
   );
 }
